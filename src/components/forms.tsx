1cffac5ae8f669e0fd9718bf3c8fb67006c59cad
Rafael Borges Bezerra <leafartech@gmail.com>
2025-03-28 20:03:14 -0300

    updt

--- src/components/forms.tsx
+++ src/components/forms.tsx
@@ -104,15 +104,15 @@ export default function Forms({ setOpen, open }: FormsType) {
                     <div className="w-full flex flex-col gap-4">
                         <div className="flex flex-col gap-2">
                             <label htmlFor="name" className="font-semibold">Digite seu nome:</label>
-                            <input value={data.name} onChange={(e) => onChange({ type: 'name', value: e.target.value })} name="name" id="name" placeholder="Nome" type="" className="border bg-transparent py-2 px-4 outline-none focus:border-zinc-500 rounded-xl text-zinc-200" />
+                            <input value={data.name} onChange={(e) => onChange({ type: 'name', value: e.target.value })} name="name" id="name" placeholder="Nome" type="" className="border bg-transparent py-2 px-4 outline-none focus:border-zinc-500 rounded-xl text-zinc-800" />
                         </div>
                         <div className="flex flex-col gap-2">
                             <label htmlFor="email" className="font-semibold">Digite seu email:</label>
-                            <input value={data.email} onChange={(e) => onChange({ type: 'email', value: e.target.value })} name="email" id="email" placeholder="exemplo@gmail.com" type="" className="border bg-transparent py-2 px-4 outline-none focus:border-zinc-500 rounded-xl text-zinc-200" />
+                            <input value={data.email} onChange={(e) => onChange({ type: 'email', value: e.target.value })} name="email" id="email" placeholder="exemplo@gmail.com" type="" className="border bg-transparent py-2 px-4 outline-none focus:border-zinc-500 rounded-xl text-zinc-800" />
                         </div>
                         <div className="flex flex-col gap-2">
                             <label htmlFor="phone" className="font-semibold">Digite seu WhatsApp:</label>
-                            <input value={data.phone} maxLength={15} onChange={(e) => onChange({ type: 'phone', value: e.target.value })} name="phone" id="phone" placeholder="(11) 99999-9999" type="" className="border bg-transparent py-2 px-4 outline-none focus:border-zinc-500 rounded-xl text-zinc-200" />
+                            <input value={data.phone} maxLength={15} onChange={(e) => onChange({ type: 'phone', value: e.target.value })} name="phone" id="phone" placeholder="(11) 99999-9999" type="" className="border bg-transparent py-2 px-4 outline-none focus:border-zinc-500 rounded-xl text-zinc-800" />
                         </div>
                     </div>
                     <button type="submit" className="w-full bg-[#44a226] rounded-lg py-4 w-[90%] text-white font-semibold">
