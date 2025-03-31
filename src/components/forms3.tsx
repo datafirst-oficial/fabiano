4955b94e0ab1fe888a4c6ca0b5ca904f62c8faaf
Rafael Borges Bezerra <leafartech@gmail.com>
2025-03-31 11:11:02 -0300

    updt

--- src/components/forms3.tsx
+++ src/components/forms3.tsx
@@ -2,22 +2,36 @@
 
 import { ChangeEvent, useMemo, useState } from "react";
 
-function Select({ label, onChange, options, placeholder, value }: { onChange: (e: ChangeEvent<HTMLSelectElement>) => void, placeholder: string, label: string, options: string[], value: string }) {
-
-    console.log(placeholder)
+function Select({ onChange, options, value, name }: { onChange: (opt: string) => void, options: string[], value: string, name: string }) {
 
     return (
-        <select
-            value={value}
-            className="w-full text-sm border border-zinc-400 outline-none text-black bg-transparent rounded-md py-3 sm:py-[6px] px-2"
-            onChange={(e: ChangeEvent<HTMLSelectElement>) => onChange(e)}
-        >
-            <option>{label}</option>
+        <div className="flex flex-col gap-1">
             {options.map((opt: string, index: number) => (
-                <option key={opt + index} value={opt}>{opt}</option>
+                <div className="flex items-center gap-1" key={index}>
+                    <input
+                        type="radio"
+                        id={opt}
+                        name={name}
+                        checked={value === opt}
+                        onChange={() => onChange(opt)}
+                    />
+                    <label htmlFor={opt}>{opt}</label>
+                </div>
             ))}
-        </select>
+        </div>
     )
+
+    // return (
+    //     <select
+    //         value={value}
+    //         className="w-full text-sm border border-zinc-400 outline-none text-black bg-transparent rounded-md py-3 sm:py-[6px] px-2"
+    //         onChange={(e: ChangeEvent<HTMLSelectElement>) => onChange(e)}
+    //     >
+    //         {options.map((opt: string, index: number) => (
+    //             <option key={opt + index} value={opt}>{opt}</option>
+    //         ))}
+    //     </select>
+    // )
 }
 
 const fields_case1 = [
@@ -28,7 +42,7 @@ const fields_case1 = [
     },
     {
         name: 'profissao',
-        type: 'text'
+        type: 'select'
 
     },
     {
@@ -200,6 +214,7 @@ export function Form3() {
     const [steps, setSteps] = useState<number>(0)
     const [data, setData] = useState<DataType>(initialData)
     const [error, setError] = useState<string>('')
+    const [submited, setSubmited] = useState<boolean>(false)
 
     const fields = useMemo(() => {
 
@@ -214,8 +229,17 @@ export function Form3() {
         ]
     }, [data])
 
-    function handleChange(e: ChangeEvent<HTMLSelectElement | HTMLInputElement>, name: keyof typeof initialData) {
+    function handleChange(e: ChangeEvent<HTMLSelectElement | HTMLInputElement> | string, name: keyof typeof initialData) {
         setError('')
+
+        if (typeof e === 'string') {
+            setData(prevState => ({
+                ...prevState,
+                [name]: e
+            }))
+
+            return
+        }
 
         setData(prevState => ({
             ...prevState,
@@ -242,6 +266,8 @@ export function Form3() {
 
             const result = await response.json();
 
+            setSubmited(true)
+
             if (!response.ok) {
                 throw new Error(result.message || 'Erro ao enviar os dados');
             }
@@ -252,83 +278,107 @@ export function Form3() {
 
     return (
         <div className="w-full">
-            <div className="flex flex-col gap-4">
-                <div className="relative">
-                    {fields.map((field, index: number) => {
-                        if (index !== steps) {
-                            return <></>
-                        } else if (field.type === 'text') {
-                            return (
-                                <input
-                                    key={index}
-                                    // @ts-expect-error: dabsd
-                                    value={data[field.name]}
-                                    maxLength={field.name === 'cellphone' ? 15 : undefined}
-                                    minLength={field.name === 'cellphone' ? 15 : undefined}
-                                    type={field.name === 'email' ? 'email' : undefined}
-                                    placeholder={labels[index]}
-                                    onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e, field.name as keyof typeof initialData)}
-                                    className="w-full text-sm border border-zinc-400 outline-none text-black bg-transparent rounded-md py-3 sm:py-[6px] px-2"
-                                />
-                            )
-                        } else {
-                            return (
-                                <Select
-                                    key={index}
-                                    // @ts-expect-error: dabsd
-                                    value={data[field.name]}
-                                    label={labels[index]}
-                                    onChange={(e: ChangeEvent<HTMLSelectElement>) => handleChange(e, field.name as keyof typeof initialData)}
-                                    options={selectOptions[field.name as keyof typeof selectOptions]}
-                                    placeholder={labels[index]}
-                                />
-                            )
-                        }
-                    })}
-                    <span className="absolute -bottom-6 left-1 w-full text-red-500 text-xs">{error}</span>
-                </div>
-                <div className="w-full flex items-center justify-between">
-                    <button
-                        type="button"
-                        className={`border text-xs uppercase text-zinc-700 flex items-center justify-center gap-1 p-2 rounded-lg ${steps === 0 && 'pointer-events-none opacity-30'}`}
-                        onClick={() => {
-                            if (steps > 0) setSteps(steps - 1)
-                        }}>
-                        <svg className="-rotate-180" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000"><path d="M646-440H200q-17 0-28.5-11.5T160-480q0-17 11.5-28.5T200-520h446L532-634q-12-12-11.5-28t11.5-28q12-12 28.5-12.5T589-691l183 183q6 6 8.5 13t2.5 15q0 8-2.5 15t-8.5 13L589-269q-12 12-28.5 11.5T532-270q-11-12-11.5-28t11.5-28l114-114Z" /></svg>
-                        {/* <span>Voltar</span> */}
-                    </button>
-                    {steps < fields.length - 1 ? (
-                        <button
-                            type="button"
-                            className={`border text-xs uppercase bg-blue-600 hover:bg-blue-700 text-white flex items-center justify-center gap-1 p-2 rounded-lg`}
-                            onClick={() => {
+            <div className="relative w-full max-w-xl py-24 px-4 sm:p-12 bg-zinc-200 z-50 rounded-xl text-black overflow-hidden">
+                {submited ? (
+                    <div className="flex flex-col items-center text-center">
+                        <h2 className="text-2xl font-semibold text-center">
+                            Inscrição concluída
+                        </h2>
+                        <svg className="w-16 h-16 fill-[#ce9a00]" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="m221-313 142-142q12-12 28-11.5t28 12.5q11 12 11 28t-11 28L250-228q-12 12-28 12t-28-12l-86-86q-11-11-11-28t11-28q11-11 28-11t28 11l57 57Zm0-320 142-142q12-12 28-11.5t28 12.5q11 12 11 28t-11 28L250-548q-12 12-28 12t-28-12l-86-86q-11-11-11-28t11-28q11-11 28-11t28 11l57 57Zm339 353q-17 0-28.5-11.5T520-320q0-17 11.5-28.5T560-360h280q17 0 28.5 11.5T880-320q0 17-11.5 28.5T840-280H560Zm0-320q-17 0-28.5-11.5T520-640q0-17 11.5-28.5T560-680h280q17 0 28.5 11.5T880-640q0 17-11.5 28.5T840-600H560Z" /></svg>
+                    </div>
+                ) : (
+                    <>
+                        <div className="mb-4 text-center">
+                            <h2 className="text-2xl font-semibold text-center">
+                                Para concluir sua inscrição, responda:
+                            </h2>
+                        </div>
+                        <div>
+                            <div className="flex flex-col gap-4">
+                                <div className="relative">
+                                    {fields.map((field, index: number) => {
+                                        if (index !== steps) {
+                                            return <></>
+                                        } else if (field.type === 'text') {
+                                            return (
+                                                <div key={index} className="flex flex-col items-start gap-1 text-left">
+                                                    <label>{labels[index]}</label>
+                                                    <input
+                                                        // @ts-expect-error: dabsd
+                                                        value={data[field.name]}
+                                                        maxLength={field.name === 'cellphone' ? 15 : undefined}
+                                                        minLength={field.name === 'cellphone' ? 15 : undefined}
+                                                        type={field.name === 'email' ? 'email' : undefined}
+                                                        placeholder={'Digite aqui...'}
+                                                        onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e, field.name as keyof typeof initialData)}
+                                                        className="w-full text-sm border border-zinc-400 outline-none text-black bg-transparent rounded-md py-3 sm:py-[6px] px-2"
+                                                    />
+                                                </div>
+                                            )
+                                        } else {
+                                            return (
+                                                <div key={index} className="flex flex-col items-start gap-1 text-left">
+                                                    <label>{labels[index]}</label>
+                                                    <Select
+                                                        key={index}
+                                                        name={field.name}
+                                                        // @ts-expect-error: dabsd
+                                                        value={data[field.name]}
+                                                        onChange={(opt: string) => handleChange(opt, field.name as keyof typeof initialData)}
+                                                        options={selectOptions[field.name as keyof typeof selectOptions]}
+                                                    />
+                                                </div>
+                                            )
+                                        }
+                                    })}
+                                    <span className="absolute -bottom-6 left-1 w-full text-red-500 text-xs">{error}</span>
+                                </div>
+                                <div className="w-full flex items-center justify-between">
+                                    <button
+                                        type="button"
+                                        className={`border text-xs uppercase text-zinc-700 flex items-center justify-center gap-1 p-2 rounded-lg ${steps === 0 && 'pointer-events-none opacity-30'}`}
+                                        onClick={() => {
+                                            if (steps > 0) setSteps(steps - 1)
+                                        }}>
+                                        <svg className="-rotate-180" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000"><path d="M646-440H200q-17 0-28.5-11.5T160-480q0-17 11.5-28.5T200-520h446L532-634q-12-12-11.5-28t11.5-28q12-12 28.5-12.5T589-691l183 183q6 6 8.5 13t2.5 15q0 8-2.5 15t-8.5 13L589-269q-12 12-28.5 11.5T532-270q-11-12-11.5-28t11.5-28l114-114Z" /></svg>
+                                        {/* <span>Voltar</span> */}
+                                    </button>
+                                    {steps < fields.length - 1 ? (
+                                        <button
+                                            type="button"
+                                            className={`border text-xs uppercase bg-blue-600 hover:bg-blue-700 text-white flex items-center justify-center gap-1 p-2 rounded-lg`}
+                                            onClick={() => {
 
-                                if (data[fields[steps].name as keyof DataType].length !== 0 && !data[fields[steps].name as keyof DataType].includes('?')) {
-                                    setSteps(prevState => prevState + 1)
-                                } else {
-                                    setError('Preencha o campo acima corretamente')
-                                }
-                            }}>
-                            {/* <span>Próximo</span> */}
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M646-440H200q-17 0-28.5-11.5T160-480q0-17 11.5-28.5T200-520h446L532-634q-12-12-11.5-28t11.5-28q12-12 28.5-12.5T589-691l183 183q6 6 8.5 13t2.5 15q0 8-2.5 15t-8.5 13L589-269q-12 12-28.5 11.5T532-270q-11-12-11.5-28t11.5-28l114-114Z" /></svg>
-                        </button>
-                    ) : (
-                        <button
-                            onClick={() => {
+                                                if (data[fields[steps].name as keyof DataType].length !== 0 && !data[fields[steps].name as keyof DataType].includes('?')) {
+                                                    setSteps(prevState => prevState + 1)
+                                                } else {
+                                                    setError('Preencha o campo acima corretamente')
+                                                }
+                                            }}>
+                                            {/* <span>Próximo</span> */}
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M646-440H200q-17 0-28.5-11.5T160-480q0-17 11.5-28.5T200-520h446L532-634q-12-12-11.5-28t11.5-28q12-12 28.5-12.5T589-691l183 183q6 6 8.5 13t2.5 15q0 8-2.5 15t-8.5 13L589-269q-12 12-28.5 11.5T532-270q-11-12-11.5-28t11.5-28l114-114Z" /></svg>
+                                        </button>
+                                    ) : (
+                                        <button
+                                            onClick={() => {
 
-                                // @ts-expect-error: dabsd
-                                if (data[fields[steps].name].includes('?')) {
-                                    setError('Preencha o campo acima corretamente')
-                                } else {
-                                    formSubmited()
-                                }
-                            }}
-                            type="submit"
-                            className={`border text-sm bg-blue-600 hover:bg-blue-700 text-white flex items-center justify-center gap-1 p-2 rounded-lg`}>
-                            <span>Enviar aplicação</span>
-                        </button>
-                    )}
-                </div>
+                                                // @ts-expect-error: dabsd
+                                                if (data[fields[steps].name].includes('?')) {
+                                                    setError('Preencha o campo acima corretamente')
+                                                } else {
+                                                    formSubmited()
+                                                }
+                                            }}
+                                            type="submit"
+                                            className={`border text-sm bg-blue-600 hover:bg-blue-700 text-white flex items-center justify-center gap-1 p-2 rounded-lg`}>
+                                            <span>Enviar aplicação</span>
+                                        </button>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                )}
             </div>
         </div>
     )
--- src/components/search.tsx
+++ src/components/search.tsx
@@ -7,17 +7,7 @@ export function Search() {
 		<div
 			className={`w-full sm:max-w-lg flex items-center justify-center`}
 		>
-			<div className="relative w-full max-w-xl py-24 px-4 sm:p-12 bg-zinc-200 z-50 rounded-xl text-black overflow-hidden">
-				<div className="mb-4 text-center">
-					<h2 className="text-2xl font-semibold text-center">
-						Para concluir sua inscrição, responda:
-					</h2>
-					{/* <p className="text-zinc-500 text-sm">Preencha as perguntas corretamente abaixo para aplicar</p> */}
-				</div>
-				<div>
-					<Form3 />
-				</div>
-			</div>
+			<Form3 />
 		</div>
 	);
 }
