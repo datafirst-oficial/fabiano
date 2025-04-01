9f3f86c5fc3c33175bd91e0e580f866233ec30f0
Rafael Borges Bezerra <leafartech@gmail.com>
2025-04-01 14:47:52 -0300

    test

--- app/api/clint/route.ts
+++ app/api/clint/route.ts
@@ -8,6 +8,25 @@ export async function POST(request: Request) {
 
     try {
 
+        await fetch(`https://dataf.app.n8n.cloud/webhook/b6ee8c6e-55ad-4e49-ac8d-7e189c8fcc25`, {
+            method: 'POST',
+            headers: {
+                "Authorization": "Bearer 2668943:46a53778646398e717d5d07ca6252f09",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email,
+                first_name: name.split(' ')[0],
+                last_name: name.split(' ')[name.split(' ').length - 1],
+                phone: Number(`55${phone.replace('(', '').replace(')', '').replace(' ', '').replace('-', '')}`),
+                utm_source,
+                utm_campaign,
+                utm_medium,
+                utm_content,
+                utm_term
+            })
+        });
+
         await fetch(`https://api.manychat.com/fb/subscriber/createSubscriber`, {
             method: 'POST',
             headers: {
@@ -24,7 +43,7 @@ export async function POST(request: Request) {
             })
         });
 
-    } catch(error) {
+    } catch (error) {
         console.log(error)
     }
 
--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -7,6 +7,29 @@ export async function POST(request: Request) {
     const { email, idade, profissao, cargo, renda, costuma_participar, opcoes_identifica, atualizado, areas_promissoras, crescer_carreira, seu_diferencial, cafe, nao_pode_faltar } = data
 
     try {
+        await fetch(`https://dataf.app.n8n.cloud/webhook/b6ee8c6e-55ad-4e49-ac8d-7e189c8fcc25`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                email,
+                idade,
+                profissao,
+                cargo,
+                renda,
+                costuma_participar,
+                opcoes_identifica,
+                atualizado,
+                areas_promissoras,
+                crescer_carreira,
+                seu_diferencial,
+                cafe,
+                nao_pode_faltar,
+            })
+        });
+
         await fetch(`https://functions-api.clint.digital/endpoints/integration/webhook/72669971-b0de-497b-bce5-eb41ea3c9bdf`, {
             method: 'POST',
             headers: {
