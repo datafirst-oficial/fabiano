20a8b98d92d6da1a885c4d1f86cf0f4379fe67b8
Rafael Borges Bezerra <leafartech@gmail.com>
2025-04-14 20:38:07 -0300

    gtm addded

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,13 +28,13 @@ export default function RootLayout({
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-')
+          })(window,document,'script','dataLayer','GTM-M7SRBSNT')
         `}} />
       </head>
       <body
         className={`${playfair_display.variable} ${inter.className}`}
       >
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-"
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M7SRBSNT"
           height="0" width="0"></iframe></noscript>
         {children}
       </body>
